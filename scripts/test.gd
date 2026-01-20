da696d807cf32e2f5a9531df436824af6e9dbe29
Styxseemsnice <styxseemsnice@gmail.com>
2026-01-20 23:39:53 +0100

    Naprawianko giery

--- scripts/test.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _process(delta):
-	var magic_on = Input.is_action_pressed("toggle_magic_sense")  # F
-
-	# wszystkie kwiaty z grupy glowing swieca
-	for obj in get_tree().get_nodes_in_group("glowing"):
-		if obj.has_method("toggle_magic_sense"):
-			obj.toggle_magic_sense(magic_on)
