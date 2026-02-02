9629594c4ef18da2638bd736f6932d5d9f320ece
oli ദ്ദി ≽^⎚˕⎚^≼ .ᐟ <oliwiakowalczyk04@onet.pl>
2026-02-03 00:17:15 +0100

    normal?

--- scripts/nature_level.gd
+++ scripts/nature_level.gd
@@ -1,1 +1,16 @@
 extends Node2D
+
+@onready var bozyszczowe_bajdurzenie: Panel = $bozyszczowe_bajdurzenie
+@onready var interakcja: Interakcja = $bozyszcze_lesne/Interakcja
+
+
+
+var active = false 
+
+func _ready() -> void:
+	interakcja.interact = Callable(self, "_on_interact")
+	if bozyszczowe_bajdurzenie:
+		bozyszczowe_bajdurzenie.visible = !bozyszczowe_bajdurzenie.visible
+func _on_interact():
+	bozyszczowe_bajdurzenie.visible = !bozyszczowe_bajdurzenie.visible
+	print("Działa")
