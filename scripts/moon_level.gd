a950ebe6754cd1afb9490b55b9ec4589415b7c54
Styxseemsnice <styxseemsnice@gmail.com>
2026-02-03 04:01:00 +0100

    SCROLLSSS PIERWSZY

--- scripts/moon_level.gd
+++ scripts/moon_level.gd
@@ -3,12 +3,17 @@ extends Node2D
 @onready var boza_wiadomosc: Panel = $boza_wiadomosc
 @onready var interakcja: Interakcja = $Bozek_ksiezyca/Interakcja
 
+
+
+
 var active = false 
 
 func _ready() -> void:
 	interakcja.interact = Callable(self, "_on_interact")
 	if boza_wiadomosc:
 		boza_wiadomosc.visible = !boza_wiadomosc.visible
+		
+		
 func _on_interact():
 	boza_wiadomosc.visible = !boza_wiadomosc.visible
 	print("Działa")
