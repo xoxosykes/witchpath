239e3910d8734719fb05e288c393b8dbae42e7e4
Styxseemsnice <styxseemsnice@gmail.com>
2026-02-08 13:22:53 +0100

    koniec

---
+++ Scrolls/scroll_gora.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var interakcja: Interakcja = $Scroll/Interakcja
+@onready var scroll_wiadomosc: Panel = $Scroll_wiadomosc
+
+var active = false 
+
+func _ready() -> void:
+	interakcja.interact = Callable(self, "_on_interact")
+	if scroll_wiadomosc:
+		scroll_wiadomosc.visible = !scroll_wiadomosc.visible
+		
+		
+func _on_interact():
+	scroll_wiadomosc.visible = !scroll_wiadomosc.visible
+	print("Działa")
